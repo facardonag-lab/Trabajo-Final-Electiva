05ff03c9c8b19d6a17c486a93d9085785ae4e4e2
facardonag <facardonag@unincca.edu.co>
2026-04-15 22:29:32 -0500

    Cambio titulo

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: isGitHubActions && repoName ? `/${repoName}` : ''
+		}
 	}
 };
 
